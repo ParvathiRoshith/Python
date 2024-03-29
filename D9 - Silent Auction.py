725068e27da5987ea6a610fdd6c256c1bfa6bacd
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-03-29 16:17:05 +0000

    update 1

--- D9 - Silent Auction.py
+++ D9 - Silent Auction.py
@@ -1,3 +1,4 @@
+from IPython.display import clear_output
 logo='''
                          ___________
                          \         /
@@ -12,3 +13,12 @@ logo='''
                        .-------------.
                       /_______________\
 '''
+print(logo)
+print("Welcome to the secret auction program!")
+
+new='yes'
+while new=='yes':
+    name=input("What is your name?: ")
+    bid_amount=int(input("Wat's your bid?: $"))
+    new=input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    clear_output(wait=True)

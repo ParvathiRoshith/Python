e84895b12039ce09f0b76fbc660438c17df6f41d
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-03-30 16:55:30 +0000

    new update

--- D9 - Silent Auction.py
+++ D9 - Silent Auction.py
@@ -1,4 +1,4 @@
-from IPython.display import clear_output
+import os
 logo='''
                          ___________
                          \         /
@@ -16,9 +16,18 @@ logo='''
 print(logo)
 print("Welcome to the secret auction program!")
 
+dict={}
+
 new='yes'
 while new=='yes':
-    name=input("What is your name?: ")
-    bid_amount=int(input("Wat's your bid?: $"))
-    new=input("Are there any other bidders? Type 'yes' or 'no'.\n")
-    clear_output(wait=True)
+  name=input("What is your name?: ")
+  bid_amount=int(input("Wat's your bid?: $"))
+  dict[name]=bid_amount
+  #print(dict)
+  new=input("Are there any other bidders? Type yes or no. \n")
+  #clear()
+  if new=='no':
+    amount=list(dict.values())
+    highest=max(amount)
+    winner=list(dict.keys())[list(dict.values()).index(highest)]
+print(f"The winner is {winner} who bid with ${highest}.")

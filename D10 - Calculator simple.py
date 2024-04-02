849ccd64e210cd30a96d3fad1136b617192530c7
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-04-02 05:48:45 +0000

    added

---
+++ D10 - Calculator simple.py
@@ -0,0 +1,52 @@
+import os
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------.
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------'
+|_____________________|
+"""
+print(logo)
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+operations={'+':add,'-':subtract,'*':multiply,'/':divide}
+
+def calculator():
+    num1=float(input("What's the first nuber?: "))
+    for key in operations:
+        print(key)
+
+    repeat='y'
+    while repeat=='y':
+        operation_symbol=input('Pick an operation: ')
+        num2=float(input("What's the next number?: "))
+        evalution=operations[operation_symbol]
+        output=evalution(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {output}")
+        repeat=input("Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation: ")
+        if repeat=='y':
+            num1=output
+        else:
+            os.system('clr')
+            calculator() #recursion
+
+calculator()

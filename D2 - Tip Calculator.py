6d0921b4b454872fca927a616e4463338500e2d7
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-04-22 16:08:04 +0000

    updated

--- D2 - Tip Calculator.py
+++ D2 - Tip Calculator.py
@@ -1,1 +1,7 @@
-print("Welcome to Python Pizza!")
+print("Welcome to the Tip Calculator!")
+bill=float(input("What was the total bill? $"))
+tip=int(input("How much tip would you like to give? 10, 12, or 15? "))
+split=int(input("How many peoplw to split the bill? "))
+total=((tip/100)*bill)+bill
+splitbill=format((total/split),".2f")
+print(f"Each person should pay: ${splitbill}")

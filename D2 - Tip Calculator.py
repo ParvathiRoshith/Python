b30752538f9fb2c4bd2e173ae906d9aaa7a5b0be
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-05-24 06:24:04 +0000

    changed

--- D2 - Tip Calculator.py
+++ D2 - Tip Calculator.py
@@ -1,7 +1,14 @@
 print("Welcome to the Tip Calculator!")
+
+#inputs
 bill=float(input("What was the total bill? $"))
 tip=int(input("How much tip would you like to give? 10, 12, or 15? "))
-split=int(input("How many peoplw to split the bill? "))
+split=int(input("How many people to split the bill? "))
+
 total=((tip/100)*bill)+bill
-splitbill=format((total/split),".2f")
-print(f"Each person should pay: ${splitbill}")
+
+#splitting the total bill among the friends
+splitbill=total/split
+roundbill=format(splitbill,".2f")  # we want output always with 2 decimal
+
+print(f"Each person should pay: ${roundbill}")

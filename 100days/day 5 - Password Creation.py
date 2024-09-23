c1e85a352b30ba68e938c4d1997e6dcaa8fc7cb6
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-09-23 08:08:39 +0000

    update

--- 100days/day 5 - Password Creation.py
+++ 100days/day 5 - Password Creation.py
@@ -1,1 +1,25 @@
-print(password)
+#Loops
+'''
+student_scores=[150,142,185,120,171,184,149,24,59,68,199,78,65,89,86]
+sum=0
+for i in student_scores:
+    sum=sum+i
+print(sum)
+
+max=0
+for score in student_scores:
+    if score>max:
+        max=score
+print(max)
+'''
+#Password Creation
+print('Welcome to the PyPassword Generator!')
+import string
+letters=list(string.ascii_letters)
+symbols=list(string.punctuation)
+num=list(string.digits)
+
+#len_letters=int(input('How many letters would you like in your password?\n'))
+#len_symbols=int(input('How many symbols would you like?\n'))
+#len_num=int(input('How many numbers would you like?\n'))
+#print(f"Your password is {output}")

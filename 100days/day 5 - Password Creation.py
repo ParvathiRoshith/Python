e5ed5435efe175d6f430fe62e3fc2784d7669e47
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2024-09-23 11:20:48 +0000

    added code

--- 100days/day 5 - Password Creation.py
+++ 100days/day 5 - Password Creation.py
@@ -1,5 +1,4 @@
 #Loops
-'''
 student_scores=[150,142,185,120,171,184,149,24,59,68,199,78,65,89,86]
 sum=0
 for i in student_scores:
@@ -11,7 +10,13 @@ for score in student_scores:
     if score>max:
         max=score
 print(max)
-'''
+
+demolist=['f', 'F', 'L', 'N', 'C', '"', '%', '7', '9', '6', '0']
+string=''
+for char in demolist:
+    string=string+char
+print(string)
+
 #Password Creation
 print('Welcome to the PyPassword Generator!')
 import string
@@ -19,7 +24,15 @@ letters=list(string.ascii_letters)
 symbols=list(string.punctuation)
 num=list(string.digits)
 
-#len_letters=int(input('How many letters would you like in your password?\n'))
-#len_symbols=int(input('How many symbols would you like?\n'))
-#len_num=int(input('How many numbers would you like?\n'))
-#print(f"Your password is {output}")
+len_letters=int(input('How many letters would you like in your password?\n'))
+len_symbols=int(input('How many symbols would you like?\n'))
+len_num=int(input('How many numbers would you like?\n'))
+
+import random
+rand_letters=random.choices(letters,k=len_letters)
+rand_symbols=random.choices(symbols,k=len_symbols)
+rand_num=random.choices(num,k=len_num)
+output=rand_letters+rand_symbols+rand_num
+random.shuffle(output)
+password=''.join(output)
+print(f"Your password is: {password}")

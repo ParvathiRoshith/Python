2d45062119cb0ff3f72a54a546d4e6c1c1abbe86
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-04-30 05:34:39 +0000

    updated

--- 100days/day 11 - Blackjack Game.py
+++ 100days/day 11 - Blackjack Game.py
@@ -10,32 +10,32 @@ logo='''
       `------'                           |__/                 
 '''
 print(logo)
-
-game=[11,2,3,4,5,6,7,8,9,10,10,10,10]
 import random
-player1=random.sample(game,2)
-print(f'Your cards: {list(player1)}')
+game=[11,2,3,4,5,6,7,8,9,10,10,10,10]
 
-player2=random.sample(game,2)
-print("Computer's first card:",player2[0])
-
-move=input("Type 'y' to get another card, type 'n' to pass: ")
-if move=='y':
-    player1=player1+random.sample(game,1)
-    '''
-elif move=='n':
-    player1=player1
-else:
-    print("You entered an invalid")'''
-
-print('Your final hand: ',player1)
-total1=sum(player1)
-print('Computer\'s final hand: ',player2)
-total2=sum(player2)
-
-if total1>total2 and total1<=21:
-    print('You Won')
-elif total1==total2:
-    print('Draw')
-else:
-    print('You Loss')
+gaming=True
+while gaming==True:
+    player1=random.sample(game,2)
+    print(f'Your cards: {list(player1)}')
+    player2=random.sample(game,2)
+    print("Computer's first card:",player2[0])
+    move=input("Type 'y' to get another card, type 'n' to pass: ")
+    if move=='y':
+        player1=player1+random.sample(game,1)
+    elif move=='n':
+        player1=player1
+    print('Your final hand: ',player1)
+    total1=sum(player1)
+    print('Computer\'s final hand: ',player2)
+    total2=sum(player2)
+    if total1>total2 and total1<=21:
+        print('You Won')
+    elif total1==total2:
+        print('Draw')
+    else:
+        print('You Loss')
+    next_game=input("Do you want play again the game of BlackJack? Type 'y' or 'n': ")
+    if next_game=='y':
+        gaming=True
+    else:
+        gaming=False

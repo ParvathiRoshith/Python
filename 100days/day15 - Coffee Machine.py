bd763448e64661ea30361fae2def22f59290e890
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-10 18:45:16 +0000

    may 11

--- 100days/day15 - Coffee Machine.py
+++ 100days/day15 - Coffee Machine.py
@@ -2,6 +2,7 @@ MENU = {
     "espresso": {
         "ingredients": {
             "water": 50,
+            "milk": 0,
             "coffee": 18,
         },
         "cost": 1.5,
@@ -26,9 +27,9 @@ MENU = {
 
 profit = 0
 resources = {
-    "water": 250,#300,
-    "milk": 100,#200,
-    "coffee": 100,#100,
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
 }
 
 def check_resource():
@@ -40,6 +41,22 @@ def check_resource():
         print("Sorry there is not enough coffee.")
     else:
         return "Process"
+    
+def check_amount():
+    global profit
+    if amount<MENU[prompt]["cost"]:
+        return "Stop"
+    elif amount>MENU[prompt]["cost"]:
+        profit=profit+MENU[prompt]["cost"]
+        change=round(amount-MENU[prompt]["cost"],2)
+        print(f"Here is ${change} dollars in change.")
+    else:
+        profit=profit+amount
+
+def deduct_resource():
+    resources['water'] = resources['water'] - MENU[prompt]["ingredients"]["water"]
+    resources['milk'] = resources['milk'] - MENU[prompt]["ingredients"]["milk"]
+    resources['coffee'] = resources['coffee'] - MENU[prompt]["ingredients"]["coffee"]
 
 coffee_machine_on=True
 while coffee_machine_on==True:
@@ -47,17 +64,21 @@ while coffee_machine_on==True:
     if prompt=='off':
         coffee_machine_on=False
     elif prompt=='report':
-        print(f"Water: {resources["water"]} \n" 
-              f"Milk: {resources["milk"]} \n" 
-              f"Coffee: {resources["coffee"]} \n"
-              f"Money: {profit}"
+        print(f"Water: {resources["water"]}ml \n" 
+              f"Milk: {resources["milk"]}ml \n" 
+              f"Coffee: {resources["coffee"]}g \n"
+              f"Money: ${profit}"
               )
     else:
         if check_resource()=="Process":
             print("Please insert coins.")
-            quarters=input("How many quarters?: ")
-            dimes=input("How many dimes?: ")
-            nickles=input("How many nickles?: ")
-            pennies=input("How many pennies?: ")
-        else:
-            coffee_machine_on=False
+            q=int(input("How many quarters?: "))
+            d=int(input("How many dimes?: "))
+            n=int(input("How many nickles?: "))
+            p=int(input("How many pennies?: "))
+            amount=(q*0.25)+(d*0.10)+(n*0.05)+(p*0.01)
+            if check_amount()=='Stop':
+                print("Sorry that's not enough money. Money refunded.")
+            else:
+                deduct_resource()
+                print(f"Here is your {prompt} Enjoy!")

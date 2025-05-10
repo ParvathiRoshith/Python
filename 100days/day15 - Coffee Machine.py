c2ee8ffcd53b9c5e1cea2eacd65bdbf995f92660
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-10 06:21:09 +0000

    new

---
+++ 100days/day15 - Coffee Machine.py
@@ -0,0 +1,44 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+coffee_machine_on=True
+while coffee_machine_on==True:
+    prompt=input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if prompt=='off':
+        coffee_machine_on=False
+    elif prompt=='report':
+        print(f"Water: {resources["water"]}" 
+              f"Milk: {resources["milk"]} \n" 
+              f"Coffee: {resources["coffee"]}"
+              f"Money: {profit}"
+              )

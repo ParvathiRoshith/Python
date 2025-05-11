1cbcf6bae85846d85b72f3e2b48d5ac3da326da4
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-11 15:41:24 +0000

    may 11

--- 100days/day16 - .py
+++ 100days/day16 - .py
@@ -1,6 +1,23 @@
 '''import turtle
 print(turtle.Turtle)'''
 
-from turtle import Turtle
-timmy=Turtle()
+'''
+from turtle import Turtle,Screen
+timmy=Turtle()            #object is the class
 print(timmy)
+
+timmy.shape("turtle")     #object.method
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen=Screen()
+print(my_screen.canvheight)   #object.attribute    ##attribute is nothing but variable in the class
+my_screen.exitonclick()
+'''
+#import prettytable      #installed external library
+from prettytable import PrettyTable               #prettytable->library  #PrettyTable->class
+table=PrettyTable()                               #the variable, table, is the object
+#print(table)
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+print(table)

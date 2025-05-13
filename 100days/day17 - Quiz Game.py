3b67e5c31e399a07d8316660171e16b17285e0bb
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-13 11:54:21 +0000

    updated

--- 100days/day17 - Quiz Game.py
+++ 100days/day17 - Quiz Game.py
@@ -57,19 +57,35 @@ class QuizBrain:
     def __init__(self,q_list):
         self.question_number=0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (Ture/False)?: ")
+        user_answer=input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer,current_question.answer)
 
     def still_has_question(self):
         if self.question_number<=len(self.question_list):
-            self.next_question
+            return True
         else:
             return False
-
+        
+    def check_answer(self,user_answer,correct_answer):
+        if user_answer.lower() == correct_answer.lower() :
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong!")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print('\n')
+            
 
 quiz=QuizBrain(question_bank)
-quiz.next_question()
-quiz.still_has_question()
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the Quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")

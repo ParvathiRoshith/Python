323ed4b6f3eff98e7cb5ff36d966aea7e895d747
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-17 15:34:24 +0000

    may 17

---
+++ 100days/day18 - Turtle.py
@@ -0,0 +1,22 @@
+''''''
+from turtle import Turtle, Screen
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)    # 90 degree angle to turn
+
+#create a square
+for i in range(4):                   
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+#darw dashed line
+for i in range(15):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+
+screen=Screen()
+screen.exitonclick()

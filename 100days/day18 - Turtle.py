50eb095b9da45420e1a09148a568c0b54c81dcf0
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-18 04:08:28 +0000

    updated

--- 100days/day18 - Turtle.py
+++ 100days/day18 - Turtle.py
@@ -19,9 +19,28 @@ for i in range(15):
     timmy_the_turtle.pendown()
 
 #draw triangle, square, pentagon, hexagone, heptagone, octagone, nonagon and decagon
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(120)
+def shape(num_of_sides):
+    angle=360/num_of_sides
+    for i in range(num_of_sides):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(angle)
+colours=["red","yellow","green","blue","violet","orange","black","grey"]
+import random
+for i in range(3,11):
+    timmy_the_turtle.color(random.sample(colours))
+    shape(i)
 
+#random walk
+import random
+timmy_the_turtle.speed("fastest")
+timmy_the_turtle.pensize(15)
+colours=["red","yellow","green","blue","violet","orange","black","grey"]
+directions=[0, 90, 180, 270]  #0: east, 90: north, 180: west, 270: south
+for i in range(200):
+    timmy_the_turtle.color(random.choice(colours))
+    timmy_the_turtle.forward(30)
+    timmy_the_turtle.setheading(random.choice(directions))
+    
 
 screen=Screen()
 screen.exitonclick()

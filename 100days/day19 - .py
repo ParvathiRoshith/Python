d8914ed4f93911fe18d80dcb6697495500cfa43e
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-05-27 03:24:22 +0000

    updated

--- 100days/day19 - .py
+++ 100days/day19 - .py
@@ -5,7 +5,26 @@ screen = Screen()
 def move_forward():
     tim.forward(10)
 
+def move_backward():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
 screen.listen()
-screen.onkey(fun=move_forward,key="space")
+screen.onkey(fun=move_forward,key="w")
+screen.onkey(fun=move_backward,key="s")
+screen.onkey(fun=turn_left,key="a")  #anticlockwise
+screen.onkey(fun=turn_right,key="d")  #clock
+screen.onkey(fun=clear,key="c")  #clear
 
 screen.exitonclick()

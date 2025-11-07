62103c0b028285d309a8f9856a434cd9f3f04f85
ParvathiRoshith <163894081+ParvathiRoshith@users.noreply.github.com>
2025-11-07 16:56:03 +0000

    new

--- Durga Sir/code_snippets.py
+++ Durga Sir/code_snippets.py
@@ -26,10 +26,20 @@ print(s[:4])
 print(s[::-1]) #reverse string
 
 # TYPE CASTING - convering one type value to another type
-#int(a) <<-- float, bool, str(integer value)
-#float(a) <<-- int, bool, str(integer/float value)
-#complex(a)/complex(a,b) <<-- int, bool, str(integer/float value)
-#bool() <<-- int, float, complex, str
-#str() <<-- int, float, complex, str
+#int(a) <<-- float, bool, str(integer value), immutable
+#float(a) <<-- int, bool, str(integer/float value), immutable
+#complex(a)/complex(a,b) <<-- int, bool, str(integer/float value), immutable
+#bool() <<-- int, float, complex, str - immutable
+#str() <<-- int, float, complex, str - immutable
 #bytes() : values must be btwn (0,2560), immutable
 #bytearray() : mutable
+#list()/[] : mutable, ordered, duplicates
+#tuple/() : immutable, ordered, duplicates
+#range() : immutable
+#set() : mutable, not ordered, no duplicates
+#frozenset({}) : immutable, not ordered, no duplicates
+#dict()/{} : mutable, not ordered, key-valuepairs (key-no duplicates, value-duplicate)
+
+#Escape charaters - \n, \t, \', \",...
+
+#OPERATIONS
